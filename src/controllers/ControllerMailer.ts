d9c1173d6ac255c366321c1e2c02ebbe2edd41a0
phe0 <pheofo@gmail.com>
2021-04-06 17:21:10 -0300

    fixclass dependencies interfaces

--- src/controllers/ControllerMailer.ts
+++ src/controllers/ControllerMailer.ts
@@ -7,35 +7,43 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default class ControllerMailer {
+	pong(req: Request, resp: Response): void {
+		const pingPong = {
+			ping: 'pong',
+		};
+		resp.status(200).json(pingPong);
+	}
 
-    pong(req: Request, resp: Response): void {
-        const pingPong = {
-            ping: "pong"
-        }
-        resp.status(200).json(pingPong);
-    }
+	sendEmail(req: Request, resp: Response): void {
+		const email = String(req.query.email);
+		const subject = String(req.query.subject);
+		const text = String(req.query.text);
+		if (
+			email !== 'undefined' &&
+			subject !== 'undefined' &&
+			text !== 'undefined'
+		) {
+			const emailMessage = this.buildEmailMessage(email, subject, text);
+			const queueService = new QueueServices();
+			const options = { attempts: 2, delay: 5000 };
+			queueService.addMailQueue(emailMessage, options);
+			queueService.emailQueueProcess();
+			resp.sendStatus(200);
+		} else {
+			resp.status(400).json({ error: 'error to get email values' });
+		}
+	}
 
-    sendEmail(req: Request, resp: Response): void {
-        const email = String(req.query.email);
-        const subject = String(req.query.subject);
-        const text = String(req.query.text);
-        if (email !== 'undefined' && subject !== 'undefined' && text !== 'undefined') {
-            const emailMessage = this.buildEmailMessage(email, subject, text);
-            const queueService = new QueueServices();
-            queueService.addMailQueue(emailMessage);
-            queueService.emailQueueProcess();
-            resp.sendStatus(200);
-        } else {
-            resp.status(400).json({'error': 'error to get email values'});
-        }
-    }
-
-    buildEmailMessage(email: string, subject: string, text: string): EmailTemplate {
-        return {
-            from: process.env.EMAIL,
-            to: email,
-            subject: subject,
-            text: text
-        }
-    }
+	buildEmailMessage(
+		email: string,
+		subject: string,
+		text: string,
+	): EmailTemplate {
+		return {
+			from: process.env.EMAIL,
+			to: email,
+			subject: subject,
+			text: text,
+		};
+	}
 }
--- src/services/QueueServices.ts
+++ src/services/QueueServices.ts
@@ -9,30 +9,31 @@ env.config();
 const { REDIS_URL } = process.env;
 
 export default class QueueServices {
-    mailerProvider: MailerProvider;
-    queue: Queue.Queue;
-    options: Record<string, unknown>;
+	mailerProvider: MailerProvider;
+	queue: Queue.Queue;
 
-    constructor() {
-        this.options = {attemps: 2, delay: 5000}
-        this.queue = new Queue('send-email-queue', REDIS_URL, this.options);
-        this.mailerProvider = new NodeMailerProvider();
-    }
+	constructor() {
+		this.queue = new Queue('send-email-queue', REDIS_URL);
+		this.mailerProvider = new NodeMailerProvider();
+	}
 
-    addMailQueue(email: EmailTemplate): void {
-        this.queue.add(email);
-    }
+	addMailQueue(email: EmailTemplate, options: Queue.JobOptions): void {
+		this.queue.add(email, options);
+	}
 
-    emailQueueProcess(): void {
-        this.queue.process(async (job) => {
-            console.log(">>>>> Process Job to Send EMAIL");
-            await this.mailerProvider.sendMail(job.data).then(() => {
-                console.log(">>>>> Finish Send Email");
-                job.moveToCompleted('done', true);
-            }).catch(err => {
-                console.log(">>>> Job Failed: " + err.message);
-                job.moveToFailed({message: 'job failed'})
-            });
-        });
-    }
+	emailQueueProcess(): void {
+		this.queue.process(async (job) => {
+			console.log('>>>>> Process Job to Send EMAIL');
+			await this.mailerProvider
+				.sendMail(job.data)
+				.then(() => {
+					console.log('>>>>> Finish Send Email');
+					job.moveToCompleted('done', true);
+				})
+				.catch((err) => {
+					console.log('>>>> Job Failed: ' + err.message);
+					job.moveToFailed({ message: 'job failed' });
+				});
+		});
+	}
 }
