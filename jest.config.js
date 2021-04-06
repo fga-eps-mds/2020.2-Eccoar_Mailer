b2620855b0171cb4354bf5347e0f547db334e8c1
phe0 <pheofo@gmail.com>
2021-04-06 16:54:48 -0300

    edit basic config files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
+  testPathIgnorePatterns: ["build"],
   testResultsProcessor: "jest-sonar-reporter",
   coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/db/"],
 };
