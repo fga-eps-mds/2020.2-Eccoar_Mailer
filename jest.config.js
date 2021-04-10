f4bf3460c3c1d517ba9295cca8a1e66459d1e5cf
devsalula <saleh.nazih.dev@gmail.com>
2021-04-10 14:38:19 -0300

    Change package name

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverage: true,
-  testPathIgnorePatterns: ["build"],
-  testResultsProcessor: "jest-sonar-reporter",
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/db/"],
-  moduleNameMapper: {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	collectCoverage: true,
+	testPathIgnorePatterns: ['build'],
+	testResultsProcessor: 'jest-sonar-reporter',
+	coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/', '/db/'],
+	moduleNameMapper: {
 		'@controllers/(.*)': '<rootDir>/src/controllers/$1',
 		'@services/(.*)': '<rootDir>/src/services/$1',
 		'@utils/(.*)': '<rootDir>/src/utils/$1',
