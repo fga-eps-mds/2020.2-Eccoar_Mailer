7377e2de33ad5d23c78bbd5dd443f90b04e36830
Brian Pina <brian.pina2001@gmail.com>
2021-04-11 16:20:39 -0300

    Change keyof Email Category

--- src/controllers/ControllerMailer.ts
+++ src/controllers/ControllerMailer.ts
@@ -9,49 +9,56 @@ import { BadRequest } from '@utils/errors';
 dotenv.config();
 
 export default class ControllerMailer {
-    
-    queueService: QueueServices;
-    
-    constructor() {
-        this.queueService = new QueueServices();
-    }
+	queueService: QueueServices;
 
-    pong(req: Request, resp: Response): void {
-        const pingPong = {
-            ping: 'pong',
-        };
-        resp.status(200).json(pingPong);
-    }
+	constructor() {
+		this.queueService = new QueueServices();
+	}
 
-    async sendEmail(req: Request, resp: Response, next: NextFunction): Promise<Response> {
-        try {
-            const reportName = String(req.body.reportName);
-            const category = String(req.body.category);
-            const location = String(req.body.location);
-            if(!reportName || !category || !location) {
-                const missingFields = [];
-                if(!reportName) missingFields.push('reportName');
-                if(!category) missingFields.push('category');
-                if(!location) missingFields.push('location');
-                throw new BadRequest(`Missing fields ${missingFields}`);
-            }
-    
-            const emailCategory = CategoryEmail[category];
-            const emailMessage = this.buildEmailMessage(location, emailCategory);
-            this.queueService.addMailQueue(emailMessage);
-            this.queueService.emailQueueProcess();
-            return resp.sendStatus(200);
-        } catch (err) {
-            next(err);
-        }
-    }
+	pong(req: Request, resp: Response): void {
+		const pingPong = {
+			ping: 'pong',
+		};
+		resp.status(200).json(pingPong);
+	}
 
-    buildEmailMessage(location: string, emailCategory: string): EmailTemplate {
-        return {
-            from: process.env.EMAIL,
-            to: emailCategory,
-            subject: `RELATÓRIO ECCOAR ${new Date().toDateString()}`,
-            text: `Aqui está o seu relatório do projeto eccoar\nLink: ${location}`
-        }
-    }
+	async sendEmail(
+		req: Request,
+		resp: Response,
+		next: NextFunction,
+	): Promise<Response> {
+		try {
+			const reportName = String(req.body.reportName);
+			const category = String(req.body.category);
+			const location = String(req.body.location);
+			if (!reportName || !category || !location) {
+				const missingFields = [];
+				if (!reportName) missingFields.push('reportName');
+				if (!category) missingFields.push('category');
+				if (!location) missingFields.push('location');
+				throw new BadRequest(`Missing fields ${missingFields}`);
+			}
+
+			const emailCategory =
+				CategoryEmail[category as keyof typeof CategoryEmail];
+			const emailMessage = this.buildEmailMessage(
+				location,
+				emailCategory,
+			);
+			this.queueService.addMailQueue(emailMessage);
+			this.queueService.emailQueueProcess();
+			return resp.sendStatus(200);
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	buildEmailMessage(location: string, emailCategory: string): EmailTemplate {
+		return {
+			from: process.env.EMAIL,
+			to: emailCategory,
+			subject: `RELATÓRIO ECCOAR ${new Date().toDateString()}`,
+			text: `Aqui está o seu relatório do projeto eccoar\nLink: ${location}`,
+		};
+	}
 }
