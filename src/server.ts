7af17f74077fa50eea311a292e03a9dc8c9e005a
MatheusBlanco <msallesblanco@gmail.com>
2021-04-17 21:01:54 -0300

    Fixing from review
    Adding sentry configuration

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,36 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 import * as morgan from 'morgan';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import routers from './routes';
 import handleErrors from './utils/errors';
+import { reqHandler, traceHandler, errHandler } from './utils/sentry';
 
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
+dotenv.config();
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	integrations: [
+		new Sentry.Integrations.Http({ tracing: true }),
+		new Tracing.Integrations.Express({ app }),
+		new Tracing.Integrations.Mysql(),
+	],
+	tracesSampleRate: 1.0,
+});
+
+app.use(reqHandler);
+app.use(traceHandler);
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(cors());
 app.use(routers);
+app.use(errHandler);
 app.use(handleErrors);
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
